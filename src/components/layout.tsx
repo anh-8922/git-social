db74fbbfb54bdf36f7839f7fb849e7bd9d072301
Anh Kim Chau <119595804+anh-8922@users.noreply.github.com>
2023-10-16 09:23:45 +0100

    update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,18 @@
+import React, { ReactNode } from 'react';
 import Heading from "./heading";
 import styles from '@/styles/index.module.css';
 import bg from '@/assets/background.jpg';
- 
-export default function Layout({ children }) {
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
-    <div className={styles.layout}
-    style={{backgroundImage: `url(${bg.src})`, backgroundSize:'cover'}}>
-      <Heading/>
+    <div className={styles.layout} style={{backgroundImage: `url(${bg.src})`, backgroundSize:'cover'}}>
+      <Heading />
       <main className={styles.main}>{children}</main>
       <p className={styles.source}>www.anh-dev.com</p>
     </div>
-  )
+  );
 }
